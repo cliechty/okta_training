d18d0d91292692ade9a8cff24e1b4b0e75319132
administrator@OKTAICE.LOCAL <administrator@OKTAICE.LOCAL>
2018-02-14 17:12:10 -0500

    Files

---
+++ OktaAuthLab/OktaAuthLab/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace OktaAuthLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult PortalHome()
+        {
+            string apiToken = ConfigurationManager.AppSettings["okta:ApiToken"];
+            var apiUrl = ConfigurationManager.AppSettings["okta:ApiUrl"];
+
+            string userId = Session["userid"].ToString();
+
+            string appUrl = apiUrl + "/api/v1/users/" + userId + "/appLinks";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(appUrl);
+
+            request.Method = "GET";
+            request.Headers.Add("Authorization", "SSWS " + apiToken);
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+
+            try
+            {
+                WebResponse response = (HttpWebResponse)request.GetResponse();
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+
+                ViewBag.apps = (JArray)JToken.ReadFrom(new JsonTextReader(reader));
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine("error: " + e.Message);
+                ModelState.AddModelError("", e.Message);
+            }
+
+            return View();
+        }
+    }
+}
